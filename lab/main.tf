b6383aa679b3b69e23977c38c50dfb55f588539b
sara <saronah1099@gmail.com>
2020-08-13 12:46:30 +0300

    fixed

--- lab/main.tf
+++ lab/main.tf
@@ -185,7 +185,7 @@ connection {
   provisioner "remote-exec" {
 
 inline =[
- "sudo echo MY IP: \"${aws_instance.api.0.public_ip}\" > /home/ubuntu/home_enginx/index.html ",
+ "sudo echo ${aws_instance.api.0.public_ip} > /home/ubuntu/home_enginx/index.html ",
 ]
 }
 }

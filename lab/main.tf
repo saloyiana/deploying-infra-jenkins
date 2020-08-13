8998a6b633fe8df1a60340d88216a3b40ffbce3f
sara <saronah1099@gmail.com>
2020-08-13 11:04:18 +0300

    .

--- lab/main.tf
+++ lab/main.tf
@@ -185,7 +185,6 @@ connection {
   provisioner "remote-exec" {
 
 inline =[
- "docker run -d -p 8080:80 --name nginx nginx",
  "sudo echo MY IP: ${aws_instance.api[count.index].public_ip} > ip.txt ",
  "cat ip.txt > /usr/share/nginx/html/index.html"
 ]
